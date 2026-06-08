a78613f4ef1d9d5e67a0a32de0c0025cdade3cfd
Shravani  Bhange <147237551+Shravani15o@users.noreply.github.com>
2026-06-08 08:46:19 +0530

    Add exception handling for string to integer conversion

---
+++ Day16_Exception.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        
+        try {
+            
+            int integerValue = Integer.parseInt(S);
+            System.out.println(integerValue);
+        } catch (Exception e) {
+            
+            System.out.println("Bad String");
+        }
+        
+        in.close();
+    }
+}
